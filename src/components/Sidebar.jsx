bd2d951d95f1a8a16fff9f3d711d87705b228833
Paurush Artwani <55234702+PaurushArtwani@users.noreply.github.com>
2025-10-01 11:05:14 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,20 @@
-const Sidebar = ({isActive}) => {
+const Sidebar = ({ isActive }) => {
   const icons = ["🏠", "🔎", "📁", "📊", "⚙️", "👤"];
-   const iconsName  = ["Home", "Search", "File", "Data", "Setting", "Your Profile"];
+  const iconsName = ["Home", "Search", "File", "Data", "Setting", "Your Profile"];
 
   return (
-   <aside className={`sidebar d-flex flex-column align-items-center py-4 ${isActive ? "active" : ""}`}>
-
-      <nav className="nav-icons d-flex flex-column gap-3">
-        {icons.map((ic, idx) => (
-          <div key={idx} className={`nav-icon ${idx === 1 ? "active" : ""}`}>
-            <span>{ic}</span>
-          <span className="label">{iconsName[idx]}</span>
-          </div>
-        ))}
-      </nav>
-    </aside>
+    <div className={`sidebar-container ${isActive ? "active" : ""}`}>
+      <aside className={`sidebar d-flex flex-column align-items-center py-4`}>
+        <nav className="nav-icons d-flex flex-column gap-3">
+          {icons.map((ic, idx) => (
+            <div key={idx} className={`nav-icon ${idx === 1 ? "active" : ""}`}>
+              <span className="">{ic}</span>
+              <span className="label ms-1">{iconsName[idx]}</span>
+            </div>
+          ))}
+        </nav>
+      </aside>
+    </div>
   );
 };
 
